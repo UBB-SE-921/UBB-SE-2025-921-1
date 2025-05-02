d3e5496debf14e334179502c748d4704f40772e5
alexapvl <alex.robite@gmail.com>
2025-05-02 12:28:26 +0300

    refactor: ProductRepository

--- Server/Repository/ProductRepository.cs
+++ Server/Repository/ProductRepository.cs
@@ -1,179 +1,134 @@
-using System;
-using System.Data;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading.Tasks;
-using SharedClassLibrary.Domain;
-using SharedClassLibrary.IRepository;
-using SharedClassLibrary.Shared;
+// <copyright file="ProductRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-// WILL NOT IMPLEMENT DUMMY PRODUCT REPOSITORY - SHOULD BE REPLACED WITH ACTUAL PRODUCT REPOSITORY -Alex
 namespace Server.Repository
 {
+    using System;
+    using System.Data;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Server.DBConnection;
+    using SharedClassLibrary.Domain;
+    using SharedClassLibrary.IRepository;
+    using SharedClassLibrary.Shared;
+
     public class ProductRepository : IProductRepository
     {
-        private readonly string connectionString;
-        private readonly IDatabaseProvider databaseProvider;
+        private readonly MarketPlaceDbContext dbContext;
 
-        [ExcludeFromCodeCoverage]
-        public ProductRepository(string connectionString)
-            : this(connectionString, new SqlDatabaseProvider())
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductRepository"/> class.
+        /// </summary>
+        /// <param name="dbContext">The database context.</param>
+        public ProductRepository(MarketPlaceDbContext dbContext)
         {
+            this.dbContext = dbContext;
         }
 
-        public ProductRepository(string connectionString, IDatabaseProvider databaseProvider)
-        {
-            if (connectionString == null)
-            {
-                throw new ArgumentNullException(nameof(connectionString));
-            }
-
-            if (databaseProvider == null)
-            {
-                throw new ArgumentNullException(nameof(databaseProvider));
-            }
-
-            this.connectionString = connectionString;
-            this.databaseProvider = databaseProvider;
-        }
-
+        /// <summary>
+        /// Adds a new product to the database.
+        /// </summary>
+        /// <param name="name">The name of the product.</param>
+        /// <param name="price">The price of the product.</param>
+        /// <param name="sellerId">The ID of the seller.</param>
+        /// <param name="productType">The type of the product.</param>
+        /// <param name="startDate">The start date of the product.</param>
+        /// <param name="endDate">The end date of the product.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public async Task AddProductAsync(string name, double price, int sellerId, string productType, DateTime startDate, DateTime endDate)
         {
-            using (IDbConnection databaseConnection = databaseProvider.CreateConnection(connectionString))
+            Product product = new Product
             {
-                using (IDbCommand databaseCommand = databaseConnection.CreateCommand())
-                {
-                    databaseCommand.CommandText = "AddProduct";
-                    databaseCommand.CommandType = CommandType.StoredProcedure;
+                ProductId = 0,
+                Name = name,
+                Description = string.Empty,
+                Price = price,
+                SellerId = sellerId,
+                Stock = 0,
+                ProductType = productType,
+                StartDate = startDate,
+                EndDate = endDate,
+            };
 
-                    AddParameter(databaseCommand, "@Name", name);
-                    AddParameter(databaseCommand, "@Price", price);
-                    AddParameter(databaseCommand, "@SellerID", sellerId);
-                    AddParameter(databaseCommand, "@ProductType", productType);
-                    AddParameter(databaseCommand, "@StartDate", startDate);
-                    AddParameter(databaseCommand, "@EndDate", endDate);
-
-                    await databaseConnection.OpenAsync();
-                    await databaseCommand.ExecuteNonQueryAsync();
-                }
-            }
+            await this.dbContext.Products.AddAsync(product);
+            await this.dbContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Updates a product in the database.
+        /// </summary>
+        /// <param name="id">The ID of the product to update.</param>
+        /// <param name="name">The new name of the product.</param>
+        /// <param name="price">The new price of the product.</param>
+        /// <param name="sellerId">The ID of the seller.</param>
+        /// <param name="productType">The type of the product.</param>
+        /// <param name="startDate">The start date of the product.</param>
+        /// <param name="endDate">The end date of the product.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="Exception">Thrown when the product is not found.</exception>
         public async Task UpdateProductAsync(int id, string name, double price, int sellerId, string productType, DateTime startDate, DateTime endDate)
         {
-            using (IDbConnection databaseConnection = databaseProvider.CreateConnection(connectionString))
-            {
-                using (IDbCommand databaseCommand = databaseConnection.CreateCommand())
-                {
-                    databaseCommand.CommandText = "UpdateProduct";
-                    databaseCommand.CommandType = CommandType.StoredProcedure;
+            Product? productToUpdate = await this.dbContext.Products.FindAsync(id)
+                    ?? throw new Exception($"UpdateProductAsync:Product not found for the product id: {id}");
 
-                    AddParameter(databaseCommand, "@ID", id);
-                    AddParameter(databaseCommand, "@Name", name);
-                    AddParameter(databaseCommand, "@Price", price);
-                    AddParameter(databaseCommand, "@SellerID", sellerId);
-                    AddParameter(databaseCommand, "@ProductType", productType);
-                    AddParameter(databaseCommand, "@StartDate", startDate);
-                    AddParameter(databaseCommand, "@EndDate", endDate);
+            productToUpdate.Name = name;
+            productToUpdate.Price = price;
+            productToUpdate.SellerId = sellerId;
+            productToUpdate.ProductType = productType;
+            productToUpdate.StartDate = startDate;
+            productToUpdate.EndDate = endDate;
 
-                    await databaseConnection.OpenAsync();
-                    await databaseCommand.ExecuteNonQueryAsync();
-                }
-            }
+            await this.dbContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Deletes a product from the database.
+        /// </summary>
+        /// <param name="id">The ID of the product to delete.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="Exception">Thrown when the product is not found.</exception>
         public async Task DeleteProduct(int id)
         {
-            using (IDbConnection databaseConnection = databaseProvider.CreateConnection(connectionString))
-            {
-                using (IDbCommand databaseCommand = databaseConnection.CreateCommand())
-                {
-                    databaseCommand.CommandText = "DeleteProduct";
-                    databaseCommand.CommandType = CommandType.StoredProcedure;
+            Product? productToDelete = await this.dbContext.Products.FindAsync(id)
+                    ?? throw new Exception($"DeleteProduct: Product not found for the product id: {id}");
 
-                    AddParameter(databaseCommand, "@ID", id);
-
-                    await databaseConnection.OpenAsync();
-                    await databaseCommand.ExecuteNonQueryAsync();
-                }
-            }
+            this.dbContext.Products.Remove(productToDelete);
+            await this.dbContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Gets the name of the seller.
+        /// </summary>
+        /// <param name="sellerId">The ID of the seller.</param>
+        /// <returns>The name of the seller.</returns>
+        /// <exception cref="Exception">Thrown when the seller is not found.</exception>
         public async Task<string> GetSellerNameAsync(int? sellerId)
         {
-            using (IDbConnection connection = databaseProvider.CreateConnection(connectionString))
+            if (sellerId == null)
             {
-                using (IDbCommand command = connection.CreateCommand())
-                {
-                    command.CommandText = "GetSellerById";
-                    command.CommandType = CommandType.StoredProcedure;
-
-                    if (sellerId.HasValue)
-                    {
-                        AddParameter(command, "@SellerID", sellerId.Value);
-                    }
-                    else
-                    {
-                        AddParameter(command, "@SellerID", DBNull.Value);
-                    }
-
-                    await connection.OpenAsync();
-
-                    object result = await command.ExecuteScalarAsync();
-                    return result?.ToString();
-                }
+                throw new Exception($"GetSellerNameAsync: Seller ID is null");
             }
+
+            Seller? seller = await this.dbContext.Sellers.FindAsync(sellerId)
+                    ?? throw new Exception($"GetSellerNameAsync: Seller not found for the seller id: {sellerId}");
+
+            return seller.StoreName;
         }
 
+        /// <summary>
+        /// Gets a product by its ID.
+        /// </summary>
+        /// <param name="productId">The ID of the product.</param>
+        /// <returns>The product.</returns>
+        /// <exception cref="Exception">Thrown when the product is not found.</exception>
         public async Task<Product> GetProductByIdAsync(int productId)
         {
-            using (IDbConnection connection = databaseProvider.CreateConnection(connectionString))
-            {
-                using (IDbCommand command = connection.CreateCommand())
-                {
-                    command.CommandText = "GetProductByID";
-                    command.CommandType = CommandType.StoredProcedure;
+            Product? product = await this.dbContext.Products.FindAsync(productId)
+                    ?? throw new Exception($"GetProductByIdAsync: Product not found for the product id: {productId}");
 
-                    AddParameter(command, "@productID", productId);
-
-                    await connection.OpenAsync();
-
-                    using (IDataReader reader = await command.ExecuteReaderAsync())
-                    {
-                        if (await reader.ReadAsync())
-                        {
-                            return new Product
-                            {
-                                ProductId = reader.GetInt32(reader.GetOrdinal("ID")),
-                                Name = reader.GetString(reader.GetOrdinal("Name")),
-                                Price = (double)reader.GetDouble(reader.GetOrdinal("Price")),
-                                SellerId = reader.IsDBNull(reader.GetOrdinal("SellerID")) ? 0 : (int)reader.GetInt32(reader.GetOrdinal("SellerID")),
-                                ProductType = reader.GetString(reader.GetOrdinal("ProductType")),
-                                StartDate = reader.IsDBNull(reader.GetOrdinal("StartDate")) ? null : (DateTime?)reader.GetDateTime(reader.GetOrdinal("StartDate")),
-                                EndDate = reader.IsDBNull(reader.GetOrdinal("EndDate")) ? null : (DateTime?)reader.GetDateTime(reader.GetOrdinal("EndDate"))
-                            };
-                        }
-
-                        return null;
-                    }
-                }
-            }
-        }
-
-        private void AddParameter(IDbCommand command, string name, object value)
-        {
-            var parameter = command.CreateParameter();
-            parameter.ParameterName = name;
-
-            if (value == null)
-            {
-                parameter.Value = DBNull.Value;
-            }
-            else
-            {
-                parameter.Value = value;
-            }
-
-            command.Parameters.Add(parameter);
+            return product;
         }
     }
 }
