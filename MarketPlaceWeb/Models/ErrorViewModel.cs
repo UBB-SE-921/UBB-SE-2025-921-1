4cd026f4e5ae8604f9ee14110cbb77c2b6c1f41e
Nicolae-Razvan Alexe <141912028+ANRazvan@users.noreply.github.com>
2025-05-15 14:10:50 +0300

    Delete MarketPlaceWeb directory

--- MarketPlaceWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MarketPlaceWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
